3cb134abd3802dd202fab1f0e1f61e60b04f2aa2
RedBirdOBX <shane.fowlkes.70@gmail.com>
2025-03-17 16:09:14 -0400

    initial check in

---
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeFactoryTests
+    {
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_DefaultSalaryMustBe2500()
+        {
+            // Arrange
+            var factory = new EmployeeFactory();
+
+            // Act
+            var employee = (InternalEmployee)factory.CreateEmployee("John", "Doe");
+
+            // Assert
+            Assert.Equal(2500, employee.Salary);
+        }
+    }
+}
